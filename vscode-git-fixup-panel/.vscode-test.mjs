1e8164ad2bd9b57681546c02a6fde366ab9b575c
netakiryosuke <sigoryosuke@gmail.com>
2026-08-04 09:23:17 +0900

    Shorten macOS test data path

--- vscode-git-fixup-panel/.vscode-test.mjs
+++ vscode-git-fixup-panel/.vscode-test.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from '@vscode/test-cli';
 
+const userDataDir = process.env.VSCODE_TEST_USER_DATA_DIR;
+
 export default defineConfig({
 	files: 'out/test/**/*.test.js',
+	launchArgs: userDataDir ? ['--user-data-dir', userDataDir] : [],
 });
